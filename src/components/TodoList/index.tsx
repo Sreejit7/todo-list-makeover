bb02c79873dda3dc3a8379278c3cb28d32cc09a0
Sreejit7 <sreejitde7@gmail.com>
2021-11-07 12:33:11 +0530

    Seperated to do lists into 'Current' and 'Completed' based on their completion status

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,23 +1,45 @@
 import styles from "./todolist.module.css";
 import { useTodoContext } from "../../contexts/TodoContext/useTodoContext";
 import TodoItem from "../TodoItem";
+import { useRef } from "react";
 
-const TodoList = () => {
+type Props = {
+  type: "current" | "completed";
+};
+const TodoList = ({ type }: Props) => {
   const {
     state: { todos },
   } = useTodoContext();
+  const todolistRef = useRef<HTMLUListElement>(null);
+  console.log(todolistRef.current);
   return (
-    <ul className={styles.todolist}>
-      {todos.map(({ task, id, borderColor, backgroundColor }) => (
-        <TodoItem
-          key={id}
-          id={id}
-          bgColor={backgroundColor}
-          borderColor={borderColor}
-          task={task}
-        />
-      ))}
-    </ul>
+    <>
+      <ul ref={todolistRef} className={styles.todolist}>
+        {todos
+          .filter(({ completed }) =>
+            type === "completed" ? completed : !completed
+          )
+          .map(({ task, id, borderColor, backgroundColor }) => (
+            <TodoItem
+              key={id}
+              id={id}
+              bgColor={backgroundColor}
+              borderColor={borderColor}
+              task={task}
+              completed={type === "completed"}
+            />
+          ))}
+      </ul>
+      {todos.filter(({ completed }) =>
+        type === "completed" ? completed : !completed
+      ).length === 0 && (
+        <span>
+          {type === "completed"
+            ? "Tick off some tasks, keep going!"
+            : "Let's plan your day, add some todos!"}
+        </span>
+      )}
+    </>
   );
 };
 
